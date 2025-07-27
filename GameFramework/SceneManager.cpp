4963b79f66e788b8f337b94f4bd4b5ac064e6351
hwangheechun <heechun3525@gmail.com>
2025-07-27 20:14:20 +0900

    DFS 구조 맵 생성(DX)

--- GameFramework/SceneManager.cpp
+++ GameFramework/SceneManager.cpp
@@ -4,11 +4,18 @@
 
 GameNode* SceneManager::_currentScene = nullptr;
 
+random_device rd;
+mt19937 g(rd());
+
 HRESULT SceneManager::Init()
 {
 	_currentScene = nullptr;
 
 	_tileImages.emplace_back(IMAGEMANAGER->AddFrameImage(L"terrain", L"Resources/tileAtlas.png", 10, 9));	// 1번
+
+	for (int y = 0; y < GRID_HEIGHT; ++y)
+		for (int x = 0; x < GRID_WIDTH; ++x)
+			roomOrder[y][x] = 0;
 
 	return S_OK;
 }
@@ -33,14 +40,20 @@ void SceneManager::Release()
 
 void SceneManager::Update()
 {
-	if (_currentScene) 
+	if (_currentScene)
+	{
 		_currentScene->Update();
+	}
 }
 
 void SceneManager::Render()
 {
 	if (_currentScene) 
 		_currentScene->Render();
+
+
+		SCENEMANAGER->ScenePlaceByDFS();
+	
 }
 
 GameNode * SceneManager::AddScene(const wstring& sceneName, GameNode* scene)
@@ -78,12 +91,29 @@ HRESULT SceneManager::ChangeScene(const wstring& sceneName)
 	return E_FAIL;
 }
 
-Image* SceneManager::GetTileImage(int tileNum)
+HRESULT SceneManager::ChangeSceneToDungeon(const wstring& sceneName)
 {
-	if (_tileImages.size() <= tileNum || tileNum < 0)
-		return nullptr;
+	mapSceneIter find = _sceneList.find(sceneName);
 
-	return _tileImages[tileNum];
+	if (find == _sceneList.end())
+		return E_FAIL;
+
+	if (find->second == _currentScene)
+		return S_OK;
+
+	if (SUCCEEDED(find->second->Init()))
+	{
+		// 어떤 Scene의 정보가 들어 있기 때문에 릴리즈 먼저 해 주고
+		if (_currentScene)
+			_currentScene->Release();
+
+		// 현재 Scene에 바꾸려는 Scene을 담는다
+		_currentScene = find->second;
+
+		return S_OK;
+	}
+
+	return E_FAIL;
 }
 
 bool SceneManager::check()
@@ -93,3 +123,62 @@ bool SceneManager::check()
 	else
 		return false;
 }
+
+Image* SceneManager::GetTileImage(int tileNum)
+{
+	if (_tileImages.size() <= tileNum || tileNum < 0)
+		return nullptr;
+
+	return _tileImages[tileNum];
+}
+
+void SceneManager::ScenePlaceByDFS()
+{
+	_D2DRenderer->RenderText(300, 1040, L"시작", 15);
+	srand(static_cast<unsigned int>(time(nullptr)));
+
+	int startX = rand() % GRID_WIDTH;
+	int startY = rand() % GRID_HEIGHT;
+
+	SceneManager::GenerateMapDFS(startX, startY);
+	SceneManager::PrintMap();
+
+}
+
+// DFS로 랜덤한 경로를 따라 방 생성 (방 수 제한)
+void SceneManager::GenerateMapDFS(int x, int y)
+{
+	if (rooms.size() >= MAX_ROOMS) return;
+
+	visited[y][x] = true;
+	rooms.emplace_back(x, y);
+	roomOrder[y][x] = rooms.size();  // 순서 저장
+
+	vector<int> dirs = { 0, 1, 2, 3 };
+	shuffle(dirs.begin(), dirs.end(), g);
+
+	for (int i = 0; i < 4; ++i) {
+		if (rooms.size() >= MAX_ROOMS) break;
+
+		int nx = x + dx[dirs[i]];
+		int ny = y + dy[dirs[i]];
+
+		if (SCENEMANAGER->isValid(nx, ny) && !visited[ny][nx]) 
+		{
+			SceneManager::GenerateMapDFS(nx, ny);
+		}
+	}
+}
+
+// 맵 출력
+void SceneManager::PrintMap()
+{
+	for (int y = 0; y < GRID_HEIGHT; ++y)
+	{
+		for (int x = 0; x < GRID_WIDTH; ++x)
+		{
+			visited[y][x] ? _D2DRenderer->RenderText(750 + x * 30, 110 + y * 30, to_wstring(roomOrder[y][x]), 15) : _D2DRenderer->RenderText(750 + x * 30, 110 + y * 30, L"X", 15);
+		}
+	}
+}
+
--- GameFramework/SceneManager.h
+++ GameFramework/SceneManager.h
@@ -1,14 +1,35 @@
 ﻿#pragma once
 #include "SingletonBase.h"
 #include <map>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#define GRID_WIDTH 4		// 맵 구조를 4x4로 설정
+#define GRID_HEIGHT 4
+#define MAX_ROOMS 8		// 최대 방 수 제한
 
 class GameNode;
+
+const int dx[] = { 0, 0, -1, 1 };
+const int dy[] = { -1, 1, 0, 0 };
 
 class SceneManager final : public SingletonBase<SceneManager>
 {
 public:
 	typedef map<wstring, GameNode*>			 mapSceneList;
 	typedef map<wstring, GameNode*>::iterator mapSceneIter;
+
+	struct Room
+	{
+		int x, y;
+		Room(int x, int y) : x(x), y(y) {}
+	};
+
+	bool visited[GRID_HEIGHT][GRID_WIDTH] = { false };
+	int roomOrder[GRID_HEIGHT][GRID_WIDTH]; // 방 생성 순서 저장
+	vector<Room> rooms;	//방 정보를 저장하는 벡터
 
 private:
 	static GameNode* _currentScene;
@@ -31,9 +52,16 @@ public:
 	GameNode* AddScene(const wstring& sceneName, GameNode* scene);
 
 	HRESULT ChangeScene(const wstring& sceneName);
+	HRESULT ChangeSceneToDungeon(const wstring& sceneName);
 
 	bool check();
 
 	Image* GetTileImage(int tileNum);
+
+	void ScenePlaceByDFS();	//방 DFS를 이용하여 생성하고 배치
+
+	bool isValid(int x, int y) { return x >= 0 && x < GRID_WIDTH && y >= 0 && y < GRID_HEIGHT; }	//방 위치를 벗어나지 않도록 체크	
+	void GenerateMapDFS(int x, int y);
+	void PrintMap();
 };
 
